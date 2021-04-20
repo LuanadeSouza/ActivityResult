06c92ad3d6f112c9ee50af8796d1a643fd106863
Luana Souza <luana.souza@ifood.com.br>
2021-04-20 14:54:35 -0300

    Obter dados de outra activity

--- app/src/main/java/br/com/luanadev/activityresult/StateListActivity.kt
+++ app/src/main/java/br/com/luanadev/activityresult/StateListActivity.kt
@@ -1,11 +1,38 @@
 package br.com.luanadev.activityresult
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.AbsListView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 
 class StateListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_state_list)
+        val listView = ListView(this)
+        setContentView(listView)
+        val stateList = resources.getStringArray(R.array.states)
+        val listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, stateList)
+        listView.adapter = listAdapter
+        listView.choiceMode = AbsListView.CHOICE_MODE_SINGLE
+        val state = intent.getStringExtra(EXTRA_STATE)
+        if (state != null){
+            val position = stateList.indexOf(state)
+            listView.setItemChecked(position, true)
+        }
+        listView.setOnItemClickListener { parent, _, position, _ ->
+            val result = parent.getItemAtPosition(position) as String
+            val intent = Intent()
+            intent.putExtra(EXTRA_RESULT, result)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+    }
+
+    companion object{
+        const val EXTRA_STATE = "estado"
+        const val EXTRA_RESULT = "result"
     }
 }
