5017d5975306b70b54a3963425ae298838601af3
Luana Souza <luana.souza@ifood.com.br>
2021-04-20 11:40:36 -0300

    create StateListActivity

---
+++ app/src/main/java/br/com/luanadev/activityresult/StateListActivity.kt
@@ -0,0 +1,11 @@
+package br.com.luanadev.activityresult
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StateListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_state_list)
+    }
+}
